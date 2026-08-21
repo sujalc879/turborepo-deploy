db9da7ec48884cd52c623572d9f937fccf65f489
sujal sunil chaudhary <sujalsunilchaudhary@sujals-Mac-mini.local>
2026-08-21 20:00:12 +0530

    add hellow usser

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,12 +5,11 @@ import { prisma } from '@repo/prisma/client'
 export default async function Home() {
   const user = await prisma.user.findFirst();
   return <div>
+    <h1>Hello User</h1>
     <div>
-
       username : {user?.username}
     </div>
     <div>
-
       password : {user?.password}
     </div>
   </div>
