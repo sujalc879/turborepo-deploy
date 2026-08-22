b40f38a6b07124f9757505a58765e307bb7ed7b8
sujal sunil chaudhary <sujalsunilchaudhary@sujals-Mac-mini.local>
2026-08-22 15:05:18 +0530

    add asia as region

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const user = await prisma.user.findFirst();
   return <div>
     <h1>Hello User</h1>
-    <h2>This is Sujal Here, From Maharastra, india</h2>
+    <h2>This is Sujal Here, From Maharastra, india, Asia</h2>
     <div>
       username : {user?.username}
     </div>
